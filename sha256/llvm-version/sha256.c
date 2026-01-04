8805a2cc056b4e32f0ed1aec2379b6c00636d51e
Lyndon <lynndon@gmail.com>
2026-01-04 14:29:06 +0800

    chore: clang-format

--- sha256/llvm-version/sha256.c
+++ sha256/llvm-version/sha256.c
@@ -1,10 +1,10 @@
 /*************************** HEADER FILES ***************************/
+#include "sha256.h"
+
 #include <memory.h>
 #include <stddef.h>
-#include <stdlib.h>
 #include <stdint.h>
-
-#include "sha256.h"
+#include <stdlib.h>
 
 /****************************** MACROS ******************************/
 #define ROTRIGHT(a, b) (((a) >> (b)) | ((a) << (32 - (b))))
@@ -31,118 +31,115 @@ static const WORD k[64] = {
     0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2};
 
 /*********************** FUNCTION DEFINITIONS ***********************/
-#define SHA256_ROUND(a, b, c, d, e, f, g, h, ki, mi) \
-  do { \
-    WORD t1 = (h) + EP1(e) + CH(e, f, g) + (ki) + (mi); \
-    WORD t2 = EP0(a) + MAJ(a, b, c); \
-    (h) = (g); \
-    (g) = (f); \
-    (f) = (e); \
-    (e) = (d) + t1; \
-    (d) = (c); \
-    (c) = (b); \
-    (b) = (a); \
-    (a) = t1 + t2; \
-  } while(0)
+#define SHA256_ROUND(a, b, c, d, e, f, g, h, ki, mi)        \
+    do {                                                    \
+        WORD t1 = (h) + EP1(e) + CH(e, f, g) + (ki) + (mi); \
+        WORD t2 = EP0(a) + MAJ(a, b, c);                    \
+        (h) = (g);                                          \
+        (g) = (f);                                          \
+        (f) = (e);                                          \
+        (e) = (d) + t1;                                     \
+        (d) = (c);                                          \
+        (c) = (b);                                          \
+        (b) = (a);                                          \
+        (a) = t1 + t2;                                      \
+    } while (0)
 
 void sha256_transform(SHA256_CTX *ctx, const BYTE data[]) {
-  WORD a, b, c, d, e, f, g, h;
-  WORD m[64];
-  const WORD *data32 = (const WORD *)data;
+    WORD a, b, c, d, e, f, g, h;
+    WORD m[64];
+    const WORD *data32 = (const WORD *)data;
 
-  for (int i = 0; i < 16; ++i) {
-    m[i] = __builtin_bswap32(data32[i]);
-  }
-  
-  #pragma GCC unroll 48
-  for (int i = 16; i < 64; ++i) {
-    m[i] = SIG1(m[i - 2]) + m[i - 7] + SIG0(m[i - 15]) + m[i - 16];
-  }
+    for (int i = 0; i < 16; ++i) {
+        m[i] = __builtin_bswap32(data32[i]);
+    }
 
-  a = ctx->state[0];
-  b = ctx->state[1];
-  c = ctx->state[2];
-  d = ctx->state[3];
-  e = ctx->state[4];
-  f = ctx->state[5];
-  g = ctx->state[6];
-  h = ctx->state[7];
+#pragma GCC unroll 48
+    for (int i = 16; i < 64; ++i) {
+        m[i] = SIG1(m[i - 2]) + m[i - 7] + SIG0(m[i - 15]) + m[i - 16];
+    }
 
-  #pragma GCC unroll 64
-  for (int i = 0; i < 64; ++i) {
-    SHA256_ROUND(a, b, c, d, e, f, g, h, k[i], m[i]);
-  }
+    a = ctx->state[0];
+    b = ctx->state[1];
+    c = ctx->state[2];
+    d = ctx->state[3];
+    e = ctx->state[4];
+    f = ctx->state[5];
+    g = ctx->state[6];
+    h = ctx->state[7];
 
-  ctx->state[0] += a;
-  ctx->state[1] += b;
-  ctx->state[2] += c;
-  ctx->state[3] += d;
-  ctx->state[4] += e;
-  ctx->state[5] += f;
-  ctx->state[6] += g;
-  ctx->state[7] += h;
+#pragma GCC unroll 64
+    for (int i = 0; i < 64; ++i) {
+        SHA256_ROUND(a, b, c, d, e, f, g, h, k[i], m[i]);
+    }
+
+    ctx->state[0] += a;
+    ctx->state[1] += b;
+    ctx->state[2] += c;
+    ctx->state[3] += d;
+    ctx->state[4] += e;
+    ctx->state[5] += f;
+    ctx->state[6] += g;
+    ctx->state[7] += h;
 }
 
 void sha256_init(SHA256_CTX *ctx) {
-  static const uint64_t initial_state[4] = {
-    0xbb67ae856a09e667ULL,
-    0xa54ff53a3c6ef372ULL,
-    0x9b05688c510e527fULL,
-    0x5be0cd191f83d9abULL
-  };
-  
-  ctx->datalen = 0;
-  ctx->bitlen = 0;
-  uint64_t *state64 = (uint64_t *)ctx->state;
-  for (int i = 0; i < 4; i++) {
-    state64[i] = initial_state[i];
-  }
+    static const uint64_t initial_state[4] = {
+        0xbb67ae856a09e667ULL, 0xa54ff53a3c6ef372ULL, 0x9b05688c510e527fULL,
+        0x5be0cd191f83d9abULL};
+
+    ctx->datalen = 0;
+    ctx->bitlen = 0;
+    uint64_t *state64 = (uint64_t *)ctx->state;
+    for (int i = 0; i < 4; i++) {
+        state64[i] = initial_state[i];
+    }
 }
 
 void sha256_update(SHA256_CTX *ctx, const BYTE data[], size_t len) {
-  WORD i;
+    WORD i;
 
-  for (i = 0; i < len; ++i) {
-    ctx->data[ctx->datalen] = data[i];
-    ctx->datalen++;
-    if (ctx->datalen == 64) {
-      sha256_transform(ctx, ctx->data);
-      ctx->bitlen += 512;
-      ctx->datalen = 0;
+    for (i = 0; i < len; ++i) {
+        ctx->data[ctx->datalen] = data[i];
+        ctx->datalen++;
+        if (ctx->datalen == 64) {
+            sha256_transform(ctx, ctx->data);
+            ctx->bitlen += 512;
+            ctx->datalen = 0;
+        }
     }
-  }
 }
 
 void sha256_final(SHA256_CTX *ctx, BYTE hash[]) {
-  WORD i;
+    WORD i;
 
-  i = ctx->datalen;
+    i = ctx->datalen;
 
-  // Pad whatever data is left in the buffer.
-  if (ctx->datalen < 56) {
-    ctx->data[i++] = 0x80;
-    while (i < 56) ctx->data[i++] = 0x00;
-  } else {
-    ctx->data[i++] = 0x80;
-    while (i < 64) ctx->data[i++] = 0x00;
+    // Pad whatever data is left in the buffer.
+    if (ctx->datalen < 56) {
+        ctx->data[i++] = 0x80;
+        while (i < 56) ctx->data[i++] = 0x00;
+    } else {
+        ctx->data[i++] = 0x80;
+        while (i < 64) ctx->data[i++] = 0x00;
+        sha256_transform(ctx, ctx->data);
+        memset(ctx->data, 0, 56);
+    }
+
+    // Append to the padding the total message's length in bits and transform.
+    ctx->bitlen += ctx->datalen * 8;
+    ctx->data[63] = ctx->bitlen;
+    ctx->data[62] = ctx->bitlen >> 8;
+    ctx->data[61] = ctx->bitlen >> 16;
+    ctx->data[60] = ctx->bitlen >> 24;
+    ctx->data[59] = ctx->bitlen >> 32;
+    ctx->data[58] = ctx->bitlen >> 40;
+    ctx->data[57] = ctx->bitlen >> 48;
+    ctx->data[56] = ctx->bitlen >> 56;
     sha256_transform(ctx, ctx->data);
-    memset(ctx->data, 0, 56);
-  }
 
-  // Append to the padding the total message's length in bits and transform.
-  ctx->bitlen += ctx->datalen * 8;
-  ctx->data[63] = ctx->bitlen;
-  ctx->data[62] = ctx->bitlen >> 8;
-  ctx->data[61] = ctx->bitlen >> 16;
-  ctx->data[60] = ctx->bitlen >> 24;
-  ctx->data[59] = ctx->bitlen >> 32;
-  ctx->data[58] = ctx->bitlen >> 40;
-  ctx->data[57] = ctx->bitlen >> 48;
-  ctx->data[56] = ctx->bitlen >> 56;
-  sha256_transform(ctx, ctx->data);
-
-  WORD *hash32 = (WORD *)hash;
-  for (i = 0; i < 8; ++i) {
-    hash32[i] = __builtin_bswap32(ctx->state[i]);
-  }
+    WORD *hash32 = (WORD *)hash;
+    for (i = 0; i < 8; ++i) {
+        hash32[i] = __builtin_bswap32(ctx->state[i]);
+    }
 }
