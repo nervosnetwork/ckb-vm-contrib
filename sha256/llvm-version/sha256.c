f435d21607105b01f2fa8becee2b0a65df041ccb
xjd <lynndon@gmail.com>
2026-01-05 09:18:29 +0800

    Optimize SHA256 without assembly code (#40)
    * refactor: add llvm version

---
+++ sha256/llvm-version/sha256.c
@@ -0,0 +1,145 @@
+/*************************** HEADER FILES ***************************/
+#include "sha256.h"
+
+/****************************** MACROS ******************************/
+#define ROTRIGHT(a, b) (((a) >> (b)) | ((a) << (32 - (b))))
+
+#define CH(x, y, z) (((x) & (y)) ^ (~(x) & (z)))
+#define MAJ(x, y, z) (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
+#define EP0(x) (ROTRIGHT(x, 2) ^ ROTRIGHT(x, 13) ^ ROTRIGHT(x, 22))
+#define EP1(x) (ROTRIGHT(x, 6) ^ ROTRIGHT(x, 11) ^ ROTRIGHT(x, 25))
+#define SIG0(x) (ROTRIGHT(x, 7) ^ ROTRIGHT(x, 18) ^ ((x) >> 3))
+#define SIG1(x) (ROTRIGHT(x, 17) ^ ROTRIGHT(x, 19) ^ ((x) >> 10))
+
+/**************************** VARIABLES *****************************/
+static const SHA256_WORD GLOBAL_K[64] = {
+    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1,
+    0x923f82a4, 0xab1c5ed5, 0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+    0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174, 0xe49b69c1, 0xefbe4786,
+    0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147,
+    0x06ca6351, 0x14292967, 0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+    0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85, 0xa2bfe8a1, 0xa81a664b,
+    0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a,
+    0x5b9cca4f, 0x682e6ff3, 0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+    0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2};
+
+/*********************** FUNCTION DEFINITIONS ***********************/
+#define SHA256_ROUND(a, b, c, d, e, f, g, h, ki, mi)               \
+    do {                                                           \
+        SHA256_WORD t1 = (h) + EP1(e) + CH(e, f, g) + (ki) + (mi); \
+        SHA256_WORD t2 = EP0(a) + MAJ(a, b, c);                    \
+        (h) = (g);                                                 \
+        (g) = (f);                                                 \
+        (f) = (e);                                                 \
+        (e) = (d) + t1;                                            \
+        (d) = (c);                                                 \
+        (c) = (b);                                                 \
+        (b) = (a);                                                 \
+        (a) = t1 + t2;                                             \
+    } while (0)
+
+void sha256_transform(SHA256_CTX *ctx, const SHA256_BYTE data[]) {
+    SHA256_WORD a, b, c, d, e, f, g, h;
+    SHA256_WORD m[64];
+    const SHA256_DWORD *data64 = (const SHA256_DWORD *)data;
+
+#pragma GCC unroll 8
+    for (int i = 0; i < 8; ++i) {
+        SHA256_DWORD temp = __builtin_bswap64(data64[i]);
+        m[2 * i] = (SHA256_WORD)(temp >> 32);
+        m[2 * i + 1] = (SHA256_WORD)temp;
+    }
+
+#pragma GCC unroll 48
+    for (int i = 16; i < 64; ++i) {
+        m[i] = SIG1(m[i - 2]) + m[i - 7] + SIG0(m[i - 15]) + m[i - 16];
+    }
+
+    a = ctx->state[0];
+    b = ctx->state[1];
+    c = ctx->state[2];
+    d = ctx->state[3];
+    e = ctx->state[4];
+    f = ctx->state[5];
+    g = ctx->state[6];
+    h = ctx->state[7];
+
+#pragma GCC unroll 64
+    for (int i = 0; i < 64; ++i) {
+        SHA256_ROUND(a, b, c, d, e, f, g, h, GLOBAL_K[i], m[i]);
+    }
+
+    ctx->state[0] += a;
+    ctx->state[1] += b;
+    ctx->state[2] += c;
+    ctx->state[3] += d;
+    ctx->state[4] += e;
+    ctx->state[5] += f;
+    ctx->state[6] += g;
+    ctx->state[7] += h;
+}
+
+void sha256_init(SHA256_CTX *ctx) {
+    static const SHA256_DWORD initial_state[4] = {
+        0xbb67ae856a09e667ULL, 0xa54ff53a3c6ef372ULL, 0x9b05688c510e527fULL,
+        0x5be0cd191f83d9abULL};
+
+    ctx->datalen = 0;
+    ctx->bitlen = 0;
+    SHA256_DWORD *state64 = (SHA256_DWORD *)ctx->state;
+    for (int i = 0; i < 4; i++) {
+        state64[i] = initial_state[i];
+    }
+}
+
+void sha256_update(SHA256_CTX *ctx, const SHA256_BYTE data[],
+                   SHA256_DWORD len) {
+    SHA256_WORD i;
+
+    for (i = 0; i < len; ++i) {
+        ctx->data[ctx->datalen] = data[i];
+        ctx->datalen++;
+        if (ctx->datalen == 64) {
+            sha256_transform(ctx, ctx->data);
+            ctx->bitlen += 512;
+            ctx->datalen = 0;
+        }
+    }
+}
+
+void sha256_final(SHA256_CTX *ctx, SHA256_BYTE hash[]) {
+    SHA256_WORD i;
+
+    i = ctx->datalen;
+
+    // Pad whatever data is left in the buffer.
+    if (ctx->datalen < 56) {
+        ctx->data[i++] = 0x80;
+        while (i < 56) ctx->data[i++] = 0x00;
+    } else {
+        ctx->data[i++] = 0x80;
+        while (i < 64) ctx->data[i++] = 0x00;
+        sha256_transform(ctx, ctx->data);
+        // memset(ctx->data, 0, 56);
+        SHA256_DWORD *data64 = (SHA256_DWORD *)ctx->data;
+        for (int j = 0; j < 7; j++) {
+            data64[j] = 0;
+        }
+    }
+
+    // Append to the padding the total message's length in bits and transform.
+    ctx->bitlen += ctx->datalen * 8;
+    SHA256_DWORD *data64 = (SHA256_DWORD *)&ctx->data[56];
+    *data64 = __builtin_bswap64(ctx->bitlen);
+    sha256_transform(ctx, ctx->data);
+
+    SHA256_DWORD *hash64 = (SHA256_DWORD *)hash;
+#pragma GCC unroll 4
+    for (i = 0; i < 4; ++i) {
+        SHA256_DWORD temp =
+            ((SHA256_DWORD)ctx->state[2 * i] << 32) | ctx->state[2 * i + 1];
+        hash64[i] = __builtin_bswap64(temp);
+    }
+}
---
+++ sha256/llvm-version/sha256.h
@@ -0,0 +1,35 @@
+/*********************************************************************
+ * Filename:   sha256.h
+ * Author:     Brad Conte (brad AT bradconte.com)
+ * Copyright:
+ * Disclaimer: This code is presented "as is" without any guarantees.
+ * Details:    Defines the API for the corresponding SHA1 implementation.
+ ********************************************************************/
+
+#ifndef SHA256_H
+#define SHA256_H
+
+/****************************** MACROS ******************************/
+#define SHA256_BLOCK_SIZE 32  // SHA256 outputs a 32 byte digest
+
+/**************************** DATA TYPES ****************************/
+// 8-bit byte
+typedef unsigned char SHA256_BYTE;
+// 32-bit word, change to "long" for 16-bit machines
+typedef unsigned int SHA256_WORD;
+// 64-bit dword
+typedef unsigned long long SHA256_DWORD;
+
+typedef struct {
+    SHA256_BYTE data[64];
+    SHA256_WORD datalen;
+    SHA256_DWORD bitlen;
+    SHA256_WORD state[8];
+} SHA256_CTX;
+
+/*********************** FUNCTION DECLARATIONS **********************/
+void sha256_init(SHA256_CTX *ctx);
+void sha256_update(SHA256_CTX *ctx, const SHA256_BYTE data[], SHA256_DWORD len);
+void sha256_final(SHA256_CTX *ctx, SHA256_BYTE hash[]);
+
+#endif  // SHA256_H
