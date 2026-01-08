09ddd93556799d358f3edad1870e5cf7330d7f12
mohanson <mohanson@outlook.com>
2026-01-08 10:44:56 +0800

    Refactor MAJ (114.8M)

--- sha256/llvm-version/sha256.c
+++ sha256/llvm-version/sha256.c
@@ -5,7 +5,7 @@
 #define ROTRIGHT(a, b) (((a) >> (b)) | ((a) << (32 - (b))))
 
 #define CH(x, y, z) (((x) & (y)) ^ (~(x) & (z)))
-#define MAJ(x, y, z) (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
+#define MAJ(x, y, z) ((((x) | (y)) & (z)) | ((x) & (y)))
 #define EP0(x) (ROTRIGHT(x, 2) ^ ROTRIGHT(x, 13) ^ ROTRIGHT(x, 22))
 #define EP1(x) (ROTRIGHT(x, 6) ^ ROTRIGHT(x, 11) ^ ROTRIGHT(x, 25))
 #define SIG0(x) (ROTRIGHT(x, 7) ^ ROTRIGHT(x, 18) ^ ((x) >> 3))
