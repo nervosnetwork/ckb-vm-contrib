d6362c603ffb6066ee65a2b705fa063581759113
Joii <joii@cryptape.com>
2026-01-23 00:57:46 +0800

    Assertions only need to be ignored under the B instruction.

--- ckb-vm-test-suite/ckb-vm-arch-test/riscv-target/ckb-vm/model_test.h
+++ ckb-vm-test-suite/ckb-vm-arch-test/riscv-target/ckb-vm/model_test.h
@@ -14,7 +14,18 @@
 //-----------------------------------------------------------------------
 
 #define RVMODEL_IO_WRITE_STR(_SP, _STR)
+
+#ifdef DISABLE_GPR_EQ
+// TODO https://github.com/riscv-non-isa/riscv-arch-test/issues/540
 #define RVMODEL_IO_ASSERT_GPR_EQ(_SP, _R, _I)
+#else
+#define RVMODEL_IO_ASSERT_GPR_EQ(_SP, _R, _I) \
+  li _SP, _I;                                 \
+  beq _R, _SP, 20002f;                        \
+  li TESTNUM, 100;                            \
+  RVTEST_FAIL;                                \
+  20002:
+#endif
 
 //-----------------------------------------------------------------------
 // RV Compliance Macros
