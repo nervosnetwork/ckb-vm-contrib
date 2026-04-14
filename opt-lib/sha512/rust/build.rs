eb9598baaf44f474faf2b9d289930cc5ea727780
mohanson <mohanson@outlook.com>
2026-04-14 14:46:13 +0800

    Remove unnecessary reference operator from clang_finder in build scripts

--- opt-lib/sha512/rust/build.rs
+++ opt-lib/sha512/rust/build.rs
@@ -4,7 +4,7 @@ fn main() {
     let target = env::var("TARGET").unwrap();
 
     let mut builder = cc::Build::new();
-    builder.compiler(&clang_finder::find());
+    builder.compiler(clang_finder::find());
     builder.file("src/sha512.c").include("src").opt_level(3);
     if target.contains("riscv64") {
         builder.flag("-march=rv64imc_zba_zbb_zbc_zbs");
